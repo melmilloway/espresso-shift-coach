e9932916656469eefb9f30e812973cb791fb7331
Melissa Milloway <mmilloway87@gmail.com>
2025-11-05 21:12:37 -0800

    Remove BrowserRouter and simplify routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Launch from "./pages/Launch";
 import Scenario from "./pages/Scenario";
 import Feedback from "./pages/Feedback";
@@ -17,17 +18,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Launch />} />
-          <Route path="/scenario" element={<Scenario />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/midshift-tip" element={<MidShiftTip />} />
-          <Route path="/shift-wrapup" element={<ShiftWrapUp />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Launch />} />
+        <Route path="/scenario" element={<Scenario />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/midshift-tip" element={<MidShiftTip />} />
+        <Route path="/shift-wrapup" element={<ShiftWrapUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
